721b0ea1876a483dc65924ff17c2ac3ffa088f92
AmauryHdsj <amaury.herbautdesaintjust@gmail.com>
2024-01-13 20:32:30 +0100

    rajout barre

--- app/Views/licencie/licencieListe.php
+++ app/Views/licencie/licencieListe.php
@@ -288,8 +288,11 @@
 <script src="vendor/jquery/jquery.min.js"></script>
 <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
+<script src="../../public/vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="../../public/vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
 
+<script src="../../public/js/demo/datatables-demo.js"></script>
 
 </body>
 </html>
